c0f617ff10e2b224f57bc8c6abe795d38ba5b989
bereket <bereketgebeyehu77@gmail.com>
2022-03-15 17:02:16 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,12 @@
 
 /**
  * print_alphabet - print alphabet lowercase
- *   
+ * 
  * Return: Always 0.
  */
 
 void print_alphabet(void)
-{	
+{
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
