ca045f4318096a79f1309aea06c5aaadd7568677
bereket <bereketgebeyehu77@gmail.com>
2022-03-17 16:59:45 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+nclude "holberton.h"
+
+
+
+/**
+ * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+
+{
+
+int i;
+
+i = 0;
+while (i < 10)
+{
+if (i != 2 && i != 4)
+{
+_putchar (i + '0');
+}
+i++
+}
+_putchar ('\n');
+
+}
