2886492a0de1f8517fb98cc09e5a388913862aa7
bereket <bereketgebeyehu77@gmail.com>
2022-03-17 14:47:30 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - chec the code
+ *
+ * Return: Always 0.
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
