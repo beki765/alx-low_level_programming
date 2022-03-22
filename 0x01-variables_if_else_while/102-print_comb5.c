20c20b38d9ae4432fd20f74ea7835a195ce37ac8
bereket <bereketgebeyehu77@gmail.com>
2022-03-22 06:36:11 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,12 +14,17 @@ while (a <= 9)
 while (b <= 9)
 {
 while (c <= 9)
-{															   while (d <= 9)
+
+{
+
+while (d <= 9)
 {															   if ((c * 10) + d > (a * 10) + b)
 {
+
 if (coma == 0)
 {
 putchar(',');
+
 putchar(32);
 }
 putchar(a + '0');													   putchar(b + '0');													      putchar(32);														 putchar(c + '0');													    putchar(d + '0');													       coma = 0;
