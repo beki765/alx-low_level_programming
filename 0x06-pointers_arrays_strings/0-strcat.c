7a2d1e3ebe861d17b9c792f550b35a0861dfdf5a
bereket <bereketgebeyehu77@gmail.com>
2022-03-24 10:40:09 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,20 +10,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-       int x = 0;
-       int y = 0;
+int x = 0;
+int y = 0;
 
-       while (dest[x])
-       {
-          x++;
-
-       }
-       while (src[y])
-       {
-
-	  dest[x] = src[y];
-          x++;
-          y++;
-       }
-       return (dest);
+while (dest[x])
+{
+x++;
+}
+while (src[y])
+{
+dest[x] = src[y];
+x++;
+y++;
+}
+return (dest);
 }
