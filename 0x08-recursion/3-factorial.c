0d853ee5b9835ed4909bd043647fd44a9f8f21d5
bereket <bereketgebeyehu77@gmail.com>
2022-03-30 10:58:04 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * factorial - function to return the factorial
+ * of n
+ *
+ * @n: parameter
+ *
+ * Return: end program
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+n = n * factorial(n - 1);
+return (n);
+}
+
+return (0);
+}
+
+© 2022 GitHub, Inc.
+
+Terms
+
+Privac
