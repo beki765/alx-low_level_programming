ddad730a69f09131480be1d09d3357316e88c0f7
bereket <bereketgebeyehu77@gmail.com>
2022-04-01 15:05:06 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Prints the multiplication of two numbers, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - Entry Point
  *
- * Return: If the program receives two arguments - 0.
- *         If the program does not receive two arguments - 1.
+ * @argc: arg counter
+ * @argv: arguments array
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-int num1, num2, prod;
+int a, b;
+(void)argc;
+(void)argv;
 
-if (argc != 3)
+
+if (argc < 3)
 {
-printf("Error\n")
-return (1);
+printf("Error\n");
+return (1)
 }
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-prod = num1 * num2;
+a = atoi(argv[1]);
+b = atoi(argv[2]);
 
-printf("%d\n", prod);
+printf("%d\n", a * b);
 
 return (0);
 }
